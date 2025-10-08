b27a061ba745fe9309f9e23dc3f1a3f809668959
sss2011 <shareefesmail12345@gmail.com>
2025-10-09 02:07:54 +0300

    funding.cpp

--- src/consensus/funding.cpp
+++ src/consensus/funding.cpp
@@ -1,4 +1,5 @@
 // Copyright (c) 2020-2023 The Zcash developers
+// Copyright (c) 2025 The Shifocoin developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or https://www.opensource.org/licenses/mit-license.php .
 
@@ -6,9 +7,19 @@
 
 namespace Consensus
 {
-CAmount FSInfo::Value(CAmount blockSubsidy) const
-{
-    // Integer division is floor division for nonnegative integers in C++
-    return CAmount((blockSubsidy * valueNumerator) / valueDenominator);
-}
+    // SHIFOCOIN CUSTOM DISTRIBUTION
+    // Founder: 14.59% of block subsidy
+    // Mining Pool: 7% of block subsidy
+    // Miners: 78.41% automatically (remainder)
+    //
+    // The valueNumerator and valueDenominator are set in chainparams.cpp
+    // for each funding stream (FS_ZIP214_BP for Founder, FS_ZIP214_ZF for Mining Pool)
+    
+    CAmount FSInfo::Value(CAmount blockSubsidy) const
+    {
+        // Integer division is floor division for nonnegative integers in C++
+        // This calculates: (blockSubsidy * numerator) / denominator
+        // Example: For 14.59% = (blockSubsidy * 1459) / 10000
+        return CAmount((blockSubsidy * valueNumerator) / valueDenominator);
+    }
 } // namespace Consensus
